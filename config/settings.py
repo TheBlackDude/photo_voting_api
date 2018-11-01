76404087d48bc7c85fedc779777bf79a6bfc692c
Ousmane Seidy Diallo <ousmane.mudu@gmail.com>
2018-11-02 02:29:27 +0800

    feat: add account model.

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
 ]
+
+AUTH_USER_MODEL = 'account.Account'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
