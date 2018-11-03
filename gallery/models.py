9ab61edf930fee71b490e236ff2316a1e6fa35cd
Ousmane Seidy Diallo <ousmane.mudu@gmail.com>
2018-11-03 20:54:52 +0800

    feat: create galley app.

---
+++ gallery/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Image(models.Model):
+    CATEGORY_CHOICES = (
+        ('people', 'People'),
+        ('nature', 'Nature'),
+        ('city_life', 'City Life'),
+        ('love', 'Love'),
+        ('sport', 'Sport'),
+        ('family', 'Family')
+    )
+    owner = models.ForeignKey(User,
+                              related_name='images', on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='Images/', default='Images/No-img.jpg')
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255, blank=True)
+    category = models.CharField(max_length=10,
+                                choices=CATEGORY_CHOICES, default='nature')
+    upvote = models.IntegerField(default=0)
+    downvote = models.IntegerField(default=0)
+    # In a real application you should use GeoDjango for GeoLocations
+    location = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
