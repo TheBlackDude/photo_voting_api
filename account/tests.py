7fc7d22add2ca268eee34df1b5a366c6bd853d8c
Ousmane Seidy Diallo <ousmane.mudu@gmail.com>
2018-11-07 05:50:27 +0800

    feat: add tests for Account API endpoints.

--- account/tests.py
+++ account/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.db import IntegrityError
-from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.core.exceptions import ValidationError
+from rest_framework.test import APITestCase
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 
 from .models import Account
@@ -56,3 +58,53 @@ class AccountModelTestCase(TestCase):
         self.assertTrue(self.get_token('test')) # now token exist
         user = Token.objects.get(key=self.get_token('test')).user
         self.assertEqual(user.first_name, account.first_name)
+
+
+class EndpointTestCase(APITestCase):
+
+    def test_login_cannot_generate_token_with_invalid_credentials(self):
+        # Create user first
+        user_details = {'email': 'test@me.com', 'username': 'test',
+                        'password': 'test123', 'confirm_password': 'test123',
+                        'first_name': 'Test', 'last_name': 'Test last'}
+        self.client.post('/api/v1/accounts/', data=user_details, format='json')
+        creds = {'username': 'test1', 'password': 'test123'}
+        user = self.client.post('/api/v1/auth/', data=creds, format='json')
+
+        self.assertFalse(user.data.get('token'))
+        self.assertEqual(user.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_generate_token_with_valid_credentials(self):
+        # Create user first
+        user_details = {'email': 'test@me.com', 'username': 'test',
+                        'password': 'test123', 'confirm_password': 'test123',
+                        'first_name': 'Test', 'last_name': 'Test last'}
+        self.client.post('/api/v1/accounts/', data=user_details, format='json')
+
+        creds = {'username': 'test', 'password': 'test123'}
+        user = self.client.post('/api/v1/auth/', data=creds, format='json')
+        self.assertEqual(user.status_code, status.HTTP_200_OK)
+        self.assertTrue(user.data.get('token'))
+        token = user.data.get('token')
+        self.assertEqual(user.data.get('token'), Token.objects.get(key=token).key)
+
+    def test_post_cannot_create_user_with_invalid_post(self):
+        user_details = {'email': 'test@me.com', 'username': '',
+                        'password': 'test123', 'confirm_password': '',
+                        'first_name': 'Test', 'last_name': 'Test last'}
+        response = self.client.post('/api/v1/accounts/', data=user_details, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['message'], 'Account could not be created with received data.')
+        # No accounts created
+        self.assertEqual(Account.objects.count(), 0)
+
+    def test_post_with_valid_data_create_user(self):
+        self.assertEqual(Account.objects.count(), 0)
+        user_details = {'email': 'test@me.com', 'username': 'test',
+                        'password': 'test123', 'confirm_password': 'test123',
+                        'first_name': 'Test', 'last_name': 'Test last'}
+        response = self.client.post('/api/v1/accounts/', data=user_details, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['msg'], 'user created successfully')
+        self.assertEqual(Account.objects.count(), 1)
+        self.assertEqual(Account.objects.first().last_name, 'Test last')
